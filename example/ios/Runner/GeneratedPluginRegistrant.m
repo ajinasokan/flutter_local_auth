3983f7c86ef0c41324bd35ee76d4e09b8f7a2b4b
Ajin Asokan <ajin.panthayil@gmail.com>
2020-09-09 11:12:37 +0530

    pub get

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<local_auth/LocalAuthPlugin.h>)
+#import <local_auth/LocalAuthPlugin.h>
+#else
+@import local_auth;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTLocalAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTLocalAuthPlugin"]];
+}
+
+@end
